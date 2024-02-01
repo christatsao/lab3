09cf7b6b7659ce323175858dd59596f059e6bfb2
Christa Tsao <tsao.christa@gmail.com>
2024-02-01 14:54:06 -0800

    Fixed bugs for reverseInPlace and reverse

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,19 +4,27 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int[] newArr = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      // System.out.println(arr[i]);
+      // System.out.println(arr[arr.length - i - 1]);
+      newArr[i] = arr[arr.length - i - 1];
+      // System.out.println(arr[i]);
+      // System.out.println(arr[arr.length - i - 1]);
+    }
+    for(int i = 0; i < newArr.length; i ++) {
+      arr[i] = newArr[i];
     }
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
   static int[] reversed(int[] arr) {
-    int[] newArray = new int[arr.length];
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+    int[] newArray = new int[arr.length]; // newArr = [0,0,0,0,0]
+    for(int i = 0; i < arr.length; i += 1) { 
+      newArray[i] = arr[arr.length - i - 1]; 
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -1,18 +1,35 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+	@Test //tells the jUnit that this is a test and we 
+  // should run it as one
+	public void testReverseInPlace() { //tests if an array with 1 int in it reverses
+    int[] input1 = { 3 }; //give an array with just 3 in it
+    ArrayExamples.reverseInPlace(input1); //call reverseinplace
+    assertArrayEquals(new int[]{ 3 }, input1); //checks if this is equal
 	}
 
 
   @Test
-  public void testReversed() {
-    int[] input1 = { };
+  public void testReversed() { //tests if an empty array is reversed
+    int[] input1 = { }; //give an array with nothing in it
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = new int[]{1,2,3,4,5};
+    assertArrayEquals(new int[]{5,4,3,2,1}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlace1() {
+    int[] input1 = new int[]{1,2,3,4,5}; 
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input1);
   }
 }
